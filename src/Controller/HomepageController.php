cad12d3134eda6b68af18846762add77a96c1ba4
Mario Nardi <system.devops@gmail.com>
2021-05-17 22:57:37 +0200

    create homepage controller
    - create homepage controller
    - add fixtures shell
    - minor change in styles.css file

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function showHomepage(): Response
+    {
+        return $this->render('homepage.html.twig');
+    }
+}
