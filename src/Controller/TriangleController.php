aefa650332c151a280e3e9dc8bcc083d5410b19f
Mario Nardi <system.devops@gmail.com>
2021-05-19 23:47:23 +0200

    Triangle calculator
    - create triangle controller
    - create triangle service
    - create result page
    - create forms for calculators
    - create triangle page

---
+++ src/Controller/TriangleController.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\TriangleService;
+
+class TriangleController extends AbstractController
+{
+    /**
+     * @Route("/triangle", name="triangle", methods={"GET"})
+     */
+    public function showTrianglePage(): Response
+    {
+        return $this->render('triangle/triangle.html.twig');
+    }
+
+     /**
+     * @Route("/triangle/base-height-area-calculator", name="base-height-area-calculator")
+     */
+    public function getBaseAndHeightParameters(Request $request, TriangleService $triangleService): Response
+    {
+        $base = (int) $request->get('base');
+        $height = (int) $request->get('height');
+
+        $triangleArea = $triangleService->baseAndHeightAreaCalculator($base, $height);
+
+        return $this->render('result_page.html.twig', [
+            'message' => TriangleService::resultMessage($triangleArea),
+        ]);
+    }
+
+    /**
+     * @Route("/triangle/heron-formula-area-calculator", name="heron-formula-area-calculator")
+     */
+    public function getSideLenghtsParameters(Request $request, TriangleService $triangleService): Response
+    {
+        $sideA = (int) $request->get('side_a');
+        $sideB = (int) $request->get('side_b');
+        $sideC = (int) $request->get('side_c');
+
+        $triangleArea = $triangleService->heronFormulaAreaCalculator($sideA, $sideB, $sideC);
+
+        return $this->render('result_page.html.twig', [
+            'message' => TriangleService::resultMessage($triangleArea),
+        ]);
+    }
+}
---
+++ src/Service/TriangleService.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace App\Service;
+
+class TriangleService
+{
+    public static function resultMessage($triangleArea)
+    {
+        return "The area of a triangle is $triangleArea square cm.";
+    }
+
+    /**
+     * Calculate area of the triangle using
+     * base and height of the triangle
+     * 
+     * Formula:
+     * a = (bh) / 2
+     */
+    public function baseAndHeightAreaCalculator(int $base, int $height): int|float
+    {
+        return ($base * $height) / 2;
+    }
+
+    /**
+     * Calculate semiperimeter of the triangle for use in Heron's formula
+     * 
+     * Formula:
+     * s = (sideA + sideB + sideC) / 2
+     */
+    public function semiperimeterCalculator(int $sideA, int $sideB, int $sideC): int|float
+    {
+        return ($sideA + $sideB + $sideC) / 2;
+    }
+
+    /**
+     * Heron's formula for calculating triangle area with semiperimeter
+     * and side lenghts
+     * 
+     * Formula:
+     * s = semiperimeter value
+     * a = sgrt(s(s-a)(s-b)(S-c))
+     */
+    public function heronFormula(int|float $semiperimeter, int $sideA, int $sideB, int $sideC): float
+    {
+        return sqrt(
+            $semiperimeter * (
+                ($semiperimeter - $sideA) * 
+                ($semiperimeter - $sideB) * 
+                ($semiperimeter - $sideC)
+            )
+        );
+    }
+
+    /**
+     * Calculate area of the triangle using Heron's formula
+     */
+    public function heronFormulaAreaCalculator(int $sideA, int $sideB, int $sideC): float
+    {
+        // calculate semiperimeter using sidelenghts
+        $semiperimeter = $this->semiperimeterCalculator($sideA, $sideB, $sideC);
+        
+        return $this->heronFormula($semiperimeter, $sideA, $sideB, $sideC);
+    }
+}
